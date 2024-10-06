cc32d43e188e91e70e187480b0e96aaddcfaa646
Saqlain Shah <saqlainshahbaltee@gmail.com>
2024-10-06 17:11:21 +0500

    update the yml file

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ app.get('/', (req, res) => {
   res.send('Hello from Yousuf Hussain Abadi Museum');
 });
 
+
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
