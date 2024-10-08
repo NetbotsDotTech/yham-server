b2842b82c05fef562b3e944d496462b0ea083439
Saqlain Shah <saqlainshahbaltee@gmail.com>
2024-10-08 11:58:19 +0500

    fix the aws bucket key issue

--- db.js
+++ db.js
@@ -1,8 +1,12 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://localhost:27017");
+    const conn = await mongoose.connect('mongodb+srv://netbotstech:yhamAdmin@cluster0.dej3w.mongodb.net/yham?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
